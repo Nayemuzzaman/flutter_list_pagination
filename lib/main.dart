3d90bee952e5de1ef181801a3a24bb832a72a8af
nayemuzzaman <nayemzaman@gmail.com>
2022-08-31 05:11:46 +0600

    basic pagination

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -13,36 +17,137 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final _baseUrl = 'https://jsonplaceholder.typicode.com/posts';
 
+  int _page = 0;
+  final int _limit = 20;
+  //1st time data loading false
+  bool _isFirstLoadRunning = false;
+  List _posts = [];
+
+  bool _hasNextPage = true;
+  bool _isLoadMoreRunning = false;
+
+  void _loadMore() async {
+    if (_hasNextPage == true &&
+        _isFirstLoadRunning == false &&
+        _isLoadMoreRunning == false) {
+      setState(() {
+        _isLoadMoreRunning = true;
+      });
+
+      _page += 1;
+
+      try {
+        final res =
+            await http.get(Uri.parse("$_baseUrl?_page=$_page&_limit=$_limit"));
+        final List fetchedPosts = json.decode(res.body);
+        if (fetchedPosts.isNotEmpty) {
+          setState(() {
+            _posts.addAll(fetchedPosts);
+          });
+        } else {
+          setState(() {
+            _hasNextPage = false;
+          });
+        }
+      } catch (err) {
+        if (kDebugMode) {
+          print('Something went wrong');
+        }
+      }
+
+      setState(() {
+        _isLoadMoreRunning = false;
+      });
+    }
+  }
+
+  void _firstLoad() async {
+    setState(() {
+      _isFirstLoadRunning = true;
+    });
+
+    try {
+      final res =
+          await http.get(Uri.parse("$_baseUrl?_page=$_page&_limit=$_limit"));
+      setState(() {
+        _posts = json.decode(res.body);
+      });
+    } catch (err) {
+      if (kDebugMode) {
+        print('Something went wrong');
+      }
+    }
+    setState(() {
+      _isFirstLoadRunning = false;
+    });
+  }
+
+  late ScrollController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //call initstate loading true until its limit
+    _firstLoad();
+    _controller = ScrollController()..addListener(_loadMore);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text(
+          'Your data',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-      body: Center(
-       
-      ),
+      body: _isFirstLoadRunning
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: _posts.length,
+                      controller: _controller,
+                      itemBuilder: (_, index) => Card(
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 10),
+                            child: ListTile(
+                              title: Text(_posts[index]['title']),
+                              subtitle: Text(_posts[index]['body']),
+                            ),
+                          )),
+                ),
+                if (_isLoadMoreRunning == true) 
+                const Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 40),
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+              ],
+            ),
     );
   }
 }
